6a87d1018466dd3ab20660d6637a7de39489a756
suharyadi <suharyadi21@outlook.com>
2023-04-13 19:40:50 +0700

    - Kuis Coding : Variabel dan Tipe Data

---
+++ quiz_variable_dan_tipe_data.js
@@ -0,0 +1,26 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai "John".
+ *  - lastName: bertipe data string, dengan nilai "Doe".
+ *  - age: bertipe data number, dengan nilai 25.
+ *  - isMarried: bertipe data boolean, dengan nilai true.
+ */
+
+// Tulis kode di bawah ini
+let firstName = "John"
+let lastName = "Doe"
+let age = 25
+let isMarried = true
+
+/*tipe data*/
+console.log(typeof(firstName))
+console.log(typeof(lastName))
+console.log(typeof(age))
+console.log(typeof(isMarried))
+
+/*nilai data*/
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(isMarried)
