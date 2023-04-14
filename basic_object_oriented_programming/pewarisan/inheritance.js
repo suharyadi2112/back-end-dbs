f5cef44cf0d32d678b1ac77891324f49bc0871a8
Suharyadi <suharyadi21@outlook.com>
2023-04-14 11:20:48 +0700

    - Create inheritance
    - Create inheritance

---
+++ basic_object_oriented_programming/pewarisan/inheritance.js
@@ -0,0 +1,54 @@
+// Superclass
+class MailService {
+  constructor(sender) {
+    this.sender = sender; 
+  }
+ 
+  sendMessage(message, receiver) { //method
+    console.log(`${this.sender} sent ${message} to ${receiver}`);
+  }
+}
+ 
+// Subclass
+class WhatsAppService extends MailService { //inheritance dari mail service
+  
+  sendBroadcastMessage(message, receivers) {
+    for (const receiver of receivers) {
+      this.sendMessage(message, receiver);
+    }
+  }
+
+}
+ 
+// Subclass
+class EmailService extends MailService { //inheritance dari mail service
+ 
+  sendDelayedMessage(message, receiver, delay) {
+    setTimeout(() => {
+      this.sendMessage(message, receiver);
+    }, delay);
+  }
+
+}
+
+const whatsapp = new WhatsAppService('+6281234567890');
+const email = new EmailService('dimas@dicoding.com');
+
+
+//bisa menggunakan method dari class MailService karena inheritance (sendMessage method milik MailService)
+whatsapp.sendMessage('Hello', '+6289876543210'); 
+
+
+whatsapp.sendBroadcastMessage('Hello', ['+6289876543210', '+6282234567890']);
+//bagian ini karena WhatsAppService tidak ada akses ke class EmailService
+whatsapp.sendDelayedMessage(); // Error //comment code ini untuk di running
+
+
+/*-----------------*/
+//bisa menggunakan method dari class MailService karena inheritance (sendMessage method milik MailService)
+email.sendMessage('Hello', 'john@doe.com');
+
+
+email.sendDelayedMessage('Hello', 'john@doe.com', 3000);
+//bagian ini karena EmailService tidak ada akses ke class WhatsAppService
+email.sendBroadcastMessage(); // Error //comment code ini untuk di running
