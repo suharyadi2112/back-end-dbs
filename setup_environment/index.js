f9f87cef743fdd4939ad78b7074505573bed88b9
suharyadi <suharyadi21@outlook.com>
2023-04-15 20:14:37 +0700

    - Update Export & Import

--- setup_environment/index.js
+++ setup_environment/index.js
@@ -1,7 +1,23 @@
-console.log("Menyalakan mesin kopi");
+/*console.log("Menyalakan mesin kopi");
 console.log("Menggiling biji kopi");
 console.log("Memanaskan air");
 console.log("Mencampurkan air dan kopi");
 console.log("Menuangkan kopi ke dalam gelas");
 console.log("Menuangkan susu ke dalam gelas");
-console.log("Kopi Anda sudah siap!");
+console.log("Kopi Anda sudah siap!");*/
+
+const coffeeStock = require('./state');
+ 
+const makeCoffee = (type, miligrams) => {
+  if (coffeeStock[type] >= miligrams) {
+    console.log("Kopi berhasil dibuat!");
+  } else {
+    console.log("Biji kopi habis!");
+  }
+}
+ 
+makeCoffee("robusta", 80);
+ 
+/* output
+Kopi berhasil dibuat!
+*/
